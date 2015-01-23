dec6e236bbd1b5cc2df999ab4f1ee2bb527d7a39
VEINHORN <mcklas@mail.ru>
2015-01-23 17:32:59 +0300

    added an ability to download file from server.

--- client.erl
+++ client.erl
@@ -35,10 +35,34 @@ send_file_binary(Socket, IoDevice, Offset) ->
       gen_tcp:send(Socket, <<2:8/integer>>) % 2 - uploading is done
   end.
 
+switch_command(Socket, Filename, Offset, Command) ->
+  case Command of
+    3 ->
+      send_file(Socket, Filename, Offset);
+    4 ->
+      io:format("download"),
+      ClientOffset = filelib:file_size("client/" ++ Filename),
+      gen_tcp:send(Socket, <<6:8/integer, ClientOffset:32/integer>>),
+      {ok, IoDevice} = file:open("client/" ++ Filename, [append]),
+      wait_for_file(Socket, IoDevice)
+  end.
+
+wait_for_file(Socket, IoDevice) ->
+  case gen_tcp:recv(Socket, 0) of
+    {ok, <<1:8/integer, Packet/binary>>} ->
+      file:write(IoDevice, Packet),
+      wait_for_file(Socket, IoDevice);
+    {ok, <<2:8/integer>>} ->
+      io:format("file downloaded.");
+    {ok, <<0:8/integer>>} ->
+      io:format("file does not exist.")
+  end.
+
 wait_for_file_info(Socket) ->
   case gen_tcp:recv(Socket, 0, ?TIMEOUT) of
-    {ok, <<Offset:32/integer, Filename/binary>>} ->
-      send_file(Socket, binary_to_list(Filename), Offset);
+    {ok, <<Offset:32/integer, Command:8/integer, Filename/binary>>} ->
+      switch_command(Socket, binary_to_list(Filename), Offset, Command);
+      %send_file(Socket, binary_to_list(Filename), Offset, Command);
     {error, timeout} ->
       ok
   end.
--- server.erl
+++ server.erl
@@ -18,12 +18,15 @@ accept(LSock) ->
 handle(Socket) ->
   case gen_tcp:recv(Socket, 0) of
     {ok, <<"DOWNLOAD ", Packet/binary>>} ->
-      io:format("~s", [Packet]),
+      Filename = delete_n(binary_to_list(Packet)),
+      Offset = filelib:file_size("server/" ++ Filename),
+      gen_tcp:send(Socket, [<<Offset:32/integer, 4:8/integer>>, Filename]),
+      wait_for_approvement(Socket, Filename),
       handle(Socket);
     {ok, <<"UPLOAD ", Packet/binary>>} ->
       Filename = delete_n(binary_to_list(Packet)),
       Offset = filelib:file_size("server/" ++ Filename),
-      gen_tcp:send(Socket, [<<Offset:32/integer>>, Filename]),
+      gen_tcp:send(Socket, [<<Offset:32/integer, 3:8/integer>>, Filename]), % if offset 0 file doesn't exist
       {ok, IoDevice} = file:open("server/" ++ Filename, [append]),
       wait_for_file(Socket, IoDevice, Offset),
       handle(Socket);
@@ -31,6 +34,35 @@ handle(Socket) ->
       handle(Socket);
     _ ->
       ok
+  end.
+
+wait_for_approvement(Socket, Filename) ->
+  case gen_tcp:recv(Socket, 0, ?TIMEOUT) of
+    {ok, <<6:8/integer, Offset:32/integer>>} ->
+      send_file(Socket, Filename, Offset),
+      ok;
+    {error, timeout} ->
+      ok
+  end.
+
+send_file(Socket, Filename, Offset) ->
+  case file:open("server/" ++ Filename, [read, binary]) of
+    {ok, IoDevice} ->
+      send_file_binary(Socket, IoDevice, Offset);
+    {error, enoent} ->
+      io:format("File does not exist~n"),
+      gen_tcp:send(Socket, <<0:8/integer>>)
+  end.
+
+send_file_binary(Socket, IoDevice, Offset) ->
+  case file:pread(IoDevice, Offset, ?CHUNK_SIZE) of
+    {ok, Data} ->
+      io:format("~B bytes sent.~n", [Offset + ?CHUNK_SIZE]),
+      gen_tcp:send(Socket, <<1:8/integer, Data/binary>>),
+      send_file_binary(Socket, IoDevice, Offset + ?CHUNK_SIZE);
+    eof ->
+      io:format("File sent.~n"),
+      gen_tcp:send(Socket, <<2:8/integer>>)
   end.
 
 wait_for_file(Socket, IoDevice, Offset) ->
