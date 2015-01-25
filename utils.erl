a5e58f38a5e0f4baed5bba259584e0d6ba2d74cd
VEINHORN <mcklas@mail.ru>
2015-01-25 13:54:55 +0300

    refactoring.

--- utils.erl
+++ utils.erl
@@ -15,16 +15,16 @@ send_file(Socket, Filename, Offset, HostFolder) ->
       utils:send_file_binary(Socket, IoDevice, Offset);
     {error, enoent} ->
       io:format("File does not exist.~n"),
-      gen_tcp:send(Socket, <<0:8/integer>>)
+      gen_tcp:send(Socket, <<?FILE_NOT_EXIST:8/integer>>)
   end.
 
 send_file_binary(Socket, IoDevice, Offset) ->
   case file:pread(IoDevice, Offset, ?CHUNK_SIZE) of
     {ok, Data} ->
       io:format("~B bytes were sent.~n", [Offset + ?CHUNK_SIZE]),
-      gen_tcp:send(Socket, <<1:8/integer, Data/binary>>),
+      gen_tcp:send(Socket, <<?SENDING:8/integer, Data/binary>>),
       send_file_binary(Socket, IoDevice, Offset + ?CHUNK_SIZE);
     eof ->
       io:format("File was sent.~n"),
-      gen_tcp:send(Socket, <<2:8/integer>>)
+      gen_tcp:send(Socket, <<?DOWNLOADED:8/integer>>)
   end.
